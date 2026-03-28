3795e3941deb73b2cf16b102045b5b4d23901f7b
DmytroLomako <lomakodmytro@gmail.com>
2026-03-28 17:07:28 +0200

    stage 2

---
+++ Week8/BoxingTester.cs
@@ -0,0 +1,21 @@
+namespace Week8;
+
+public static class BoxingTester
+{
+    public static void Test()
+    {
+        int a = 10; 
+        
+        object b = a; 
+        
+        int c = (int)b; 
+        
+        Console.WriteLine($"a: {a}");
+        Console.WriteLine($"b: {b}");
+        Console.WriteLine($"c:  {c}");
+  
+        // створено int а, яка зберігається у stack
+        // новий об'єкт b додається до heap (boxing) і зберігає посилання на значення а 
+        // витягується змінна з heap і вставляється у с (unboxing)
+    }
+}
