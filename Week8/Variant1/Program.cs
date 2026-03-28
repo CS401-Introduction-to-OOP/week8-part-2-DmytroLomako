698f72d68e5d7620eed41ba10f5a22d806fc2b63
DmytroLomako <lomakodmytro@gmail.com>
2026-03-28 16:56:35 +0200

    stage 3

--- Week8/Variant1/Program.cs
+++ Week8/Variant1/Program.cs
@@ -8,12 +8,19 @@ class Program
         var file = new FileResource("report.txt");
         var network = new NetworkResource("api.company.local");
         var manager = new ResourceManager<Resource>();
+
+        manager.Add(file);
+        manager.Add(network);
+
+        manager.OpenAll();
+
+        using(var resource = new FileResource("temp_file")) 
+        {
+            resource.Open();
+        }
         
-        // TODO:
-        // 1. Add resources to manager
-        // 2. Open all resources
-        // 3. Demonstrate using with one disposable resource
-        // 4. Close all resources
+        manager.CloseAll();
+
         Console.WriteLine("Done.");
     }
 }
